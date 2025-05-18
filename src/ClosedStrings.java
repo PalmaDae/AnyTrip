327c1275c1a5179779552ce2fadf380a8e139a91
PalmaDae <arturd277@mail.ru>
2025-05-18 15:16:19 +0300

    tg bot

---
+++ src/ClosedStrings.java
@@ -0,0 +1,3 @@
+public final class ClosedStrings {
+    public static final String TOKEN = "7278366558:AAGu5jqJTPi46g6eHjKH28yQ01bIwgRT1Ic";
+}
---
+++ src/Runner.java
@@ -0,0 +1,14 @@
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+
+public class Runner {
+    public static void main(String[] args) {
+        String botToken = ClosedStrings.TOKEN;
+        try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+            botsApplication.registerBot(botToken, new MyBot(botToken));
+            System.out.println("Psst, i see dead people");
+            Thread.currentThread().join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
